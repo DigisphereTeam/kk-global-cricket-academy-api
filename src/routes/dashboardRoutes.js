dff109fce54545e3078ea462331343f447b6b5ed
dev-naveen-1729 <Digisheretech@1729>
2026-08-04 17:16:33 +0530

    feat: update dashboard revenue and activity statistics API

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { getDashboardStatistics } = require("../controllers/dashboardController");
+const { getDashboardStatistics, getDashboardCharts, getDashboardRevenueAndActivities } = require("../controllers/dashboardController");
 const verifyToken = require("../middlewares/verifyToken");
 
 const dashboardRouter = express.Router();
@@ -8,5 +8,9 @@ const dashboardRouter = express.Router();
 dashboardRouter.use(verifyToken);
 
 dashboardRouter.get("/overview", getDashboardStatistics);
+
+dashboardRouter.get("/charts", getDashboardCharts);
+
+dashboardRouter.get("/revenue-activities", getDashboardRevenueAndActivities);
 
 module.exports = dashboardRouter;
